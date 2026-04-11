c03b245926f58a21469c0e996d7ba6489de21660
nathanlaan <722624+NathanLaan@users.noreply.github.com>
2026-04-11 14:06:05 -0600

    feat: NoteLiner icon.

--- src/main/main.js
+++ src/main/main.js
@@ -50,6 +50,7 @@ function createWindow() {
     height: 800,
     minWidth: 800,
     minHeight: 600,
+    icon: path.join(__dirname, '..', '..', 'assets', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
